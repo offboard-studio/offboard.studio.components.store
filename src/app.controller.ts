97b9e41bae1c395f9e815a0620652d79d01f30c0
harunkurtdev <harunkurt.dev@gmail.com>
2025-06-25 23:16:42 +0300

    deploy:vercel project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  
+  @Get("json")
+  getjson(): string {
+    return this.appService.jsonData();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { collectionBlocks, getCollectionBlock } from 'assets/collection/collection-factory';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  jsonData(): any {
+    return getCollectionBlock('drivers.control.motorDriver');
   }
 }
